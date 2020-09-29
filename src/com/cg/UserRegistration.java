e8df20ab3980f66490cbf9d3c3dce42ee5c004fd
Arijit20 <71620955+Arijit20@users.noreply.github.com>
2020-09-29 16:28:57 +0530

    Update UserRegistration.java

--- src/com/cg/UserRegistration.java
+++ src/com/cg/UserRegistration.java
@@ -38,7 +38,7 @@ public class UserRegistration {
 		System.out.println("Enter your Email :");
 		String email = in.next();
 		Pattern pattern = Pattern
-				.compile("^([a-z]){1,}([.+_-]){0,1}[0-9a-z]*(@){1}([0-9a-z]*)(\\.([a-z]){2,}){1}(\\.[a-z][a-z])?$");
+				.compile("^([a-z]){1,}[a-z0-9]*([.+_-]){0,1}[0-9a-z]+(@){1}([0-9a-z]+)(\\.([a-z]){2,}){1}(\\.[a-z]{2,})?$");
 		Matcher matcher = pattern.matcher(email);
 		Boolean b = matcher.find();
 		if (b)
